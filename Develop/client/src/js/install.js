d16a7d42c7701097ac7f398c09fc84cebc3a93c1
Abdikalik Dini <adini18@gmail.com>
2023-10-14 14:45:38 -0400

    added logic to butinstall

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -10,7 +10,18 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        console.log('no deferred prompt available');
+        return;
+    }
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+    butInstall.classList.add('hidden');
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    
+});
