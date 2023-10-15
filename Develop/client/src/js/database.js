80833205e0e0e2ab5a643a39294a49d4cfae2559
Abdikalik Dini <adini18@gmail.com>
2023-10-15 16:57:21 -0400

    added functions for getdb and opendb

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+  export const putDb = async (content) => {
+    try {
+      const db = await openDB('jate', 1);
+      const tx = db.transaction('jate', 'readwrite');
+      tx.store.put(content);
+      await tx.done;
+    } catch (error) {
+      console.error('Failed to put content into db:', error);
+    }
+  };
+  
+  export const getDb = async () => {
+    try {
+      const db = await openDB('jate', 1);
+      return await db.getAll('jate');
+    } catch (error) {
+      console.error('Failed to get content from db:', error);
+      throw error; 
+    }
+  };
+  
 initdb();
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,15 +1,11 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    console.log('beforeinstallprompt event:'+ event)
-    event.preventDefault();
-    window.deferredPrompt = event;
-    butInstall.classList.remove('hidden');
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
     if (!promptEvent) {
@@ -21,7 +17,6 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.add('hidden');
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log('app installed:', event);
     butInstall.classList.add('hidden');
